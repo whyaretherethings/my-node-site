5d87a95bfb893b9ddbf1a2626d0574c20fdbab68
Your Full Name <youremail@example.com>
2026-03-13 08:06:23 -0600

    Added pages and navigation

--- server.js
+++ server.js
@@ -7,12 +7,23 @@ const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
+
 app.get('/', (req, res) => {
-    res.send("<h1>Welcome to My Website</h1>")
+  res.sendFile(__dirname + '/public/index.html')
 })
+
 app.get('/about', (req, res) => {
-    res.send("<h1>About me</h1><p>This page describes me.</p>")
+  res.sendFile(__dirname + '/public/about.html')
 })
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html')
+})
+
+app.get('/quizzes', (req, res) => {
+  res.sendFile(__dirname + '/public/quizzes.html')
+})
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
 })
